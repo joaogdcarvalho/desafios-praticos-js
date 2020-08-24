77cc9aa14ed19b066f3ffc24ca8b6447212dca40
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-08-24 14:28:25 -0300

    App de Todos | Excluindo todos

--- _aula03/todos.js
+++ _aula03/todos.js
@@ -10,14 +10,27 @@ var todos = [
 
 function renderTodos() {
 
-  //limpeza de todos os itens da lista, antes de renderizar
+  // limpeza de todos os itens da lista, antes de renderizar
   listElement.innerHTML = '';
 
   for (todo of todos) {
     var todoElement = document.createElement('li');
     var todoText = document.createTextNode(todo);
 
+    var linkElement = document.createElement('a');
+    linkElement.setAttribute('href', '#');
+
+    var pos = todos.indexOf(todo);
+
+    // ao clicar no link, será removido item da lista
+    linkElement.setAttribute('onClick', 'deleteTodo(' + pos + ')');
+
+    var linkText = document.createTextNode("Excluir");
+
+    linkElement.appendChild(linkText);
+
     todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
     listElement.appendChild(todoElement);
   }
 }
@@ -36,3 +49,11 @@ function addTodo() {
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+
+  // removerá quantidades de itens de array, conforme posição inicial indicada
+  todos.splice(pos, 1);
+
+  renderTodos();
+}
